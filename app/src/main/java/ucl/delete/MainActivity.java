b163a2bc97b1446b1f0af4f2c9804bbc2b708c4b
Yihantee94 <yihantee94@gmail.com>
2016-03-19 13:26:27 +0000

    Initial commit

---
+++ app/src/main/java/ucl/delete/MainActivity.java
@@ -0,0 +1,40 @@
+package ucl.delete;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    EditText editText;
+    Button addButton;
+    ListView listView;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //generate list
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("item1");
+        list.add("item2");
+
+        //instantiate custom adapter
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.my_listview);
+        lView.setAdapter(adapter);
+    }
+}
